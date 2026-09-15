5b7094122b0d71998f5157297d1ee9df16e6ee9b
yaumil <yaumilramkhadani@gmail.com>
2026-09-15 14:57:58 +0700

    feat : get data habit

--- app/src/main/java/yr/muhammadyaumil/taskflow/presentations/ui/ManageHabits/HabitsRoute.kt
+++ app/src/main/java/yr/muhammadyaumil/taskflow/presentations/ui/ManageHabits/HabitsRoute.kt
@@ -1,15 +1,25 @@
 package yr.muhammadyaumil.taskflow.presentations.ui.ManageHabits
 
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 
 @Serializable
-object HabitsRoute : NavKey
+data object HabitsRoute : NavKey
 
 fun EntryProviderScope<NavKey>.habitsRoute(backstack: NavBackStack<NavKey>) {
     entry<HabitsRoute> {
-        ManageHabits()
+        val viewModel: HabitViewModel = hiltViewModel()
+        val uiState by viewModel.uiState.collectAsState()
+        
+        ManageHabits(
+            isLoading = uiState.isLoading,
+            todayHabitList = uiState.todayHabits,
+            allHabitList = uiState.allHabits
+        )
     }
 }

c2efb1460fa3c8e7c16dcb6129cd0ed4ba80f950
yaumil <yaumilramkhadani@gmail.com>
2026-09-15 14:57:48 +0700

    feat : get data habit

--- app/src/main/java/yr/muhammadyaumil/taskflow/data/ManageHabits/dataSources/HabitRemoteDataSource.kt
+++ app/src/main/java/yr/muhammadyaumil/taskflow/data/ManageHabits/dataSources/HabitRemoteDataSource.kt
@@ -4,20 +4,21 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import jakarta.inject.Inject
 import kotlinx.coroutines.tasks.await
-import yr.muhammadyaumil.taskflow.core.response.Response
 import yr.muhammadyaumil.taskflow.data.ManageHabits.models.HabitDto
 
-
 interface HabitRemoteDataSource {
-    suspend fun addHabit(habit: HabitDto): Response<Unit>
+    suspend fun addHabit(habit: HabitDto)
+    suspend fun getHabit(): List<HabitDto>
 }
 
 class HabitRemoteDataSourceImpl @Inject constructor(
     private val firestore: FirebaseFirestore,
     private val auth: FirebaseAuth
 ) : HabitRemoteDataSource {
-    override suspend fun addHabit(habit: HabitDto): Response<Unit> {
+
+    override suspend fun addHabit(habit: HabitDto) {
         val userId = auth.currentUser?.uid ?: throw Exception("User Belum Login")
+
         val documentRef = firestore
             .collection("users")
             .document(userId)
@@ -26,7 +27,20 @@ class HabitRemoteDataSourceImpl @Inject constructor(
 
         val habitWithId = habit.copy(id = documentRef.id)
         documentRef.set(habitWithId).await()
-        return Response.Success(Unit)
     }
 
+    override suspend fun getHabit(): List<HabitDto> {
+        val userId = auth.currentUser?.uid ?: throw Exception("User Belum Login")
+
+        val querySnapshot = firestore
+            .collection("users")
+            .document(userId)
+            .collection("habits")
+            .get()
+            .await()
+
+        return querySnapshot.documents.mapNotNull { document ->
+            document.toObject(HabitDto::class.java)
+        }
+    }
 }
